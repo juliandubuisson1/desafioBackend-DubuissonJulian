f8554f65966d5c86a9ec7b76acdb085f14eb6513
juliandubuisson1 <juliandubuisson1@gmail.com>
2024-07-11 18:29:39 -0300

    modificaciones generales

--- src/utils.js
+++ src/utils.js
@@ -13,6 +13,12 @@ export const JWT_SECRET = process.env.jwtSecret;
 export const CLIENT_ID = process.env.clientId;
 export const CLIENT_SECRET = process.env.clientSecret;
 export const CALLBACK_URL = process.env.callbackURL;
+export const EMAIL_USERNAME = process.env.MAIL_USERNAME;
+export const EMAIL_PASSWORD = process.env.MAIL_PASSWORD;
+export const TWILIO_SSID = process.env.TWILIO_SSID;
+export const AUTH_TOKEN = process.env.AUTH_TOKEN;
+export const PHONE_NUMBER = process.env.PHONE_NUMBER;
+export const PHONE_NUMBER_TO = process.env.PHONE_NUMBER_TO;
 
 export function getProductsFilePath() {
     return path.join(__dirname, "../productos.json");
@@ -25,7 +31,7 @@ export function getCartFilePath() {
 export function configureProductMulter() {
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, path.join(__dirname, 'public', 'img'));
+            cb(null, path.join(__dirname, 'public', 'products'));
         },
         filename: function (req, file, cb) {
             cb(null, file.originalname);
@@ -33,6 +39,42 @@ export function configureProductMulter() {
     });    
 
     return multer({ storage: storage });
+}
+
+export function configureProfileMulter() {
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, path.join(__dirname, 'public', 'profiles'));
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        },
+    });    
+
+    return multer({ storage: storage });
+}
+
+export function configureDocumentMulter() {
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, path.join(__dirname, 'public', 'documents'));
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        },
+    });    
+
+    return multer({ storage: storage });
+}
+
+export function generateRandomCode(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 export default __dirname;
